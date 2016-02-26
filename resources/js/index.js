c970440b60ee9e5070692ace81f6d03cf94ccb0a
sachakilinski <100s100@gmail.com>
2016-02-26 15:51:39 -0300

    evolution

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,16 +3,21 @@ requirejs.config({
 });
 
 requirejs(['vendors/backbone.js'], function(){
-    var Todolist =  Backbone.Model.extend({})
-    var todolist = new Todolist({title: 'title'})
-    var TodolistView = Backbone.View.extend({
+    var Todoitem =  Backbone.Model.extend({urlRoot: 'http://private-1ae31-backboneapi.apiary-mock.com/todos/'})
+    var todoitem = new Todoitem({id: 770 })
+    console.log(JSON.stringify(todoitem))
+    var TodoitemView = Backbone.View.extend({
         render: function(){
             var html = '<li>' + this.model.get('title') + '</li>'
             $(this.el).html(html)
         }
     })
-    var todolistView = new TodolistView({model: todolist})
-    todolistView.render()
-    $('#app').html(todolistView.el)
+    var todoitemView = new TodoitemView({model: todoitem})
+
+
+    todoitem.fetch({success: function(){
+        todoitemView.render()
+        $('#app').html(todoitemView.el)
+    }})
 })
 
