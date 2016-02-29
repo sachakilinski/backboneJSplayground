236a0bce2b0bfc748f03ca230d3592fecadce060
sachakilinski <100s100@gmail.com>
2016-02-29 19:47:10 -0300

    added intialization and event

--- resources/js/index.js
+++ resources/js/index.js
@@ -6,14 +6,23 @@ requirejs(['vendors/backbone.js'], function(){
     var Todoitems =  Backbone.Collection.extend({url: 'http://private-1ae31-backboneapi.apiary-mock.com/todos'})
     var todoitems = new Todoitems()
     var TodoitemsView = Backbone.View.extend({
+        initialize: function(){
+            var html = '<input type="text" name="newtodo" id="newtodo" value="Add Item"> <input type="submit" id="addtodo" value="Add">'
+            $(this.el).html(html)
+        },
+        events:{
+          "click" : 'addTodo'
+        },
         renderItem: function(item){
             var html = '<li>' + item.get('title') + '</li>'
-            $(todoitemsView.el).append(html)
+            $(this.el).append(html)
         },
         render: function(){
-            this.collection.each(this.renderItem)
+            this.collection.each(this.renderItem, this)
+        },
+        addTodo: function(e){
+            alert("clicked")
         }
-
     })
     var todoitemsView = new TodoitemsView({collection: todoitems})
 
